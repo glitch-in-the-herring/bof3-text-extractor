62c16693f25be58af53c037a530c5eb47c15e875
Red Herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-07-04 15:18:15 +0700

    Update bofscript.py

--- bofscript.py
+++ bofscript.py
@@ -8,11 +8,11 @@ parser.add_argument("-o", "--output", help="Output to this file. If not specifie
 
 args = parser.parse_args()
 
-printables = {0x01 : '\n', 0xff : ' ', 0x5d : '!', 0x90 : '"', 0x8e : "'", 0x3c : ',', 0x3d : '-', 0x3e : '.', 0x5c : '?', 0x00 : '\n\n', 0x02: '\n\u25bc\n'}
+printables = {0x01 : '\n', 0xff : ' ', 0x5d : '!', 0x90 : '"', 0x8e : "'", 0x3c : ',', 0x3d : '-', 0x3e : '.', 0x5c : '?', 0x00 : '\n\n------\n', 0x20 : '\n\n------\n', 0x02: '\n\u25bc\n'}
 window_position = {0xe4 : '\u2196', 0xe5 : '\u2197', 0xe6 : '\u2199', 0xe7 : '\u2198'}
 controls = [
                 {b'\x0c\x03' : b'\xe4', b'\x0c\x04' : b'\xe5', b'\x0c\x05' : b'\xe6', b'\x0c\x06' : b'\xe7'}, 
-                {b'\x0d' : b'<', b'\x0f[\x00-\x09]' : b'>', b'\x0b' : b'--'},
+                {b'\x0d' : b'\xff', b'\x0f[\x00-\x09]' : b'\xff', b'\x0b' : b'--'},
                 {b'\x04\x00' : b'Ryu', b'\x04\x01' : b'Nina', b'\x04\x02' : b'Garr', b'\x04\x03': b'Teepo', b'\x04\x04' : b'Rei', b'\x04\x05' : b'Momo', b'\x04\x06' : b'Peco'}
             ]
 alpha = list(range(65, 91)) + list(range(97, 123))
@@ -27,6 +27,7 @@ def main() -> int:
     if args.output:
         output_file = open(args.output, 'w', encoding='utf-8')
         output_address = 0
+        output_file.seek(0)
 
     area_file.seek(8)
     if area_file.read(8) != b'MATH_TBL':
@@ -35,44 +36,43 @@ def main() -> int:
 
     area_file.seek(0)
     chunk = area_file.read(512)
+    dialogue_chunk = b''
     prepad_chunks_found = [False, False]
     final_chunk =  False
     i = 0
-    j = 0
 
-    while len(chunk) > 0 and not final_chunk:
+    while not final_chunk:
         if not (prepad_chunks_found[0] and prepad_chunks_found[1]):
             if chunk == b'\x00' * 136 + b'\x5f' * 376 and not prepad_chunks_found[1]:
                 prepad_chunks_found[0] = True
             elif chunk == b'\x5f' * 512 and prepad_chunks_found[0]:
                 prepad_chunks_found[1] = True
         else:
-            output = ''
+            dialogue_chunk += chunk
             if re.search(b'_{2,}$', chunk):
                 final_chunk = True
 
-            for table in controls:
-                for byte, item in table.items():
-                    chunk = re.sub(byte, item, chunk)
-            
-            for byte in chunk:
-                if byte in alpha:
-                    output += chr(byte)
-                elif byte in printables:
-                    output += printables[byte]
-                elif byte in window_position:
-                    output += window_position[byte]
-
-            if args.output:        
-               output_address += output_file.write(output)
-               output_file.seek(output_address)
-            else:
-                print(output)
-                
         i += 1
-        address = area_file.seek(512 * i)
+        area_file.seek(512 * i)
         chunk = area_file.read(512)
-   
+
+    for table in controls:
+        for byte, item in table.items():
+            dialogue_chunk = re.sub(byte, item, dialogue_chunk)
+
+    output = ''
+    for byte in dialogue_chunk:
+        if byte in alpha:
+            output += chr(byte)
+        elif byte in printables:
+            output += printables[byte]
+        elif byte in window_position:
+            output += window_position[byte]
+
+    if args.output:
+        output_file.write(str(output))
+    else:
+        print(output)  
     
     area_file.close()
     if args.output:

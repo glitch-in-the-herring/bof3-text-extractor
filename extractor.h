ced4dfe77edd10c270e1f632b3305aa6874694c8
Red Herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-07-05 12:14:43 +0700

    Add files via upload

---
+++ extractor.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+typedef uint8_t byte;
+typedef struct node
+{
+    byte chunk[512];
+    struct node *next;
+} node;
+
+void free_node(node *n);
+void copy_arrays(byte target[], byte source[], int n);
+bool is_math_tbl(byte chunk[]);
+bool is_first_prepadding(byte chunk[]);
+bool is_second_prepadding(byte chunk[]);
+bool is_final_chunk(byte chunk[]);

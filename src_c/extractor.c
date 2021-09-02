a90440e562410cad1328d0d4e785dab586e811a7
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-09-03 05:21:46 +0700

    begin work on toc scan

--- src_c/extractor.c
+++ src_c/extractor.c
@@ -9,104 +9,29 @@ int main(int argc, char *argv[])
     }
 
     FILE *area_file = fopen(argv[1], "rb");
-    FILE *output_file = fopen(argv[2], "w");
     if (area_file == NULL)
     {
         printf("Error opening input file\n");
-        fclose(output_file);
-        remove(argv[2]);
         return 2;
     }
-    else if (output_file == NULL)
-    {
-        printf("Error opening output file\n");
-        fclose(area_file);
-        return 3;
-    }
 
-    byte chunk[512];
-    node *chunk_chain = NULL;
-    node *temp = calloc(1, sizeof(node));
-    if (temp == NULL)
-    {
-        printf("Failed to allocate memory\n");
-        fclose(area_file);
-        fclose(output_file);
-        remove(argv[2]);
-        return 4;
-    }
-    chunk_chain = temp;
-
-    int chunk_count = 0;
-    bool final_chunk = false;
-    bool first_chunk = true;
-    bool paddings_found[2] = {false, false};
-
+    byte toc_entries[16];
     fread(chunk, 1, sizeof(chunk), area_file);
     if (!is_math_tbl(chunk))
     {
         printf("Not a valid .EMI file!\n");
-        fclose(area_file);
-        fclose(output_file);
+        fclose(area_file);;
         free_node(chunk_chain);
-        remove(argv[2]);
         return 5;
     }
 
-    while (fread(chunk, 1, sizeof(chunk), area_file) > 0 && !final_chunk)
+    FILE *output_file = fopen(argv[2], "w");
+    if (output_file == NULL)
     {
-        if (!(paddings_found[0] && paddings_found[1]))
-        {
-            if (!paddings_found[1] && is_first_prepadding(chunk))
-            {
-                paddings_found[0] = true;
-            }         
-            else if (paddings_found[0] && is_second_prepadding(chunk))
-            {
-                paddings_found[1] = true;
-            }
-        }
-        else
-        {
-            chunk_count++;
-            temp->next = calloc(1, sizeof(node));
-            if (temp->next == NULL)
-            {
-                printf("Failed to allocate memory\n");
-                fclose(area_file);
-                fclose(output_file);
-                free_node(chunk_chain);
-                remove(argv[2]);
-                return 4;
-            }
-            copy_arrays(temp->chunk, chunk, 512);
-            temp = temp->next;
-            // Check if this is the final chunk.
-            final_chunk = is_final_chunk(chunk);
-        }
-    }
-
-    if (!(paddings_found[0] && paddings_found[1]))
-    {
-        printf("No dialogue section found in this .EMI file!\n");
+        printf("Error opening output file\n");
         fclose(area_file);
-        fclose(output_file);
-        free_node(chunk_chain);
-        remove(argv[2]);
-        return 6;
-    }
-
-    byte dialogue_section[512 * chunk_count];
-    int i = 0;
-    for (node *n = chunk_chain; n->next != NULL; n = n->next)
-    {
-        for (int j = 0; j < 512; j++)
-        {
-            dialogue_section[512 * i + j] = n->chunk[j];
-        }
-        i++;
-    }
-    free_node(chunk_chain);
+        return 3;
+    }    
 
     char punct;
     char last_color[8];

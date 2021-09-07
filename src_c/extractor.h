8fd260d5389d03244d12d021a1de9b3c4db02b34
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-09-07 19:33:45 +0700

    function prototypes didn't match

--- src_c/extractor.h
+++ src_c/extractor.h
@@ -9,7 +9,7 @@ typedef uint32_t word;
 
 word convert_little_endian(byte a[], int n, int k);
 word find_dialogue_section(FILE *f, word count, word *section_size);
-bool is_math_tbl(byte chunk[]);
+bool is_math_tbl(byte toc_header[]);
 bool is_dialogue_section(byte toc_entry[]);
 bool is_alpha(byte a);
 char is_punct(byte a);
--- src_c/jpextractor.h
+++ src_c/jpextractor.h
@@ -16,7 +16,7 @@ typedef uint32_t word;
 word convert_little_endian(byte a[], int n, int k);
 word find_dialogue_section(FILE *f, word count, word *section_size);
 void load_lookup_table(int n, char table[][n], FILE *source);
-bool is_math_tbl(byte chunk[]);
+bool is_math_tbl(byte toc_header[]);
 bool is_dialogue_section(byte toc_entry[]);
 bool is_hiragana(byte a);
 bool is_katakana(byte a);
